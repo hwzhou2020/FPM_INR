d013def09a9b3e638b7cdbfd43f0b3774ed45b13
Haowen Zhou <hzhou7@caltech.edu>
2023-06-28 23:54:54 -0700

    Model save function is written.

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib as mpl
 from matplotlib.colors import ListedColormap
+import torch
+
+def save_model_with_required_grad(model, save_path):
+    tensors_to_save = []
+    
+    # Traverse through model parameters and append tensors with require_grad=True to the list
+    for param_name, param_tensor in model.named_parameters():
+        if param_tensor.requires_grad:
+            tensors_to_save.append(param_tensor)
+    
+    # Save the list of tensors
+    torch.save(tensors_to_save, save_path)
 
 newcolors = np.vstack(
     (
