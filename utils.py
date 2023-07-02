e7f9736a1347ae829e74b3295e228166a2193117
Haowen Zhou <hzhou7@caltech.edu>
2023-07-02 16:00:16 -0700

    Add load model with reuired_grad parameters

--- utils.py
+++ utils.py
@@ -14,6 +14,15 @@ def save_model_with_required_grad(model, save_path):
     # Save the list of tensors
     torch.save(tensors_to_save, save_path)
 
+def load_model_with_required_grad(model, load_path):
+    # Load the list of tensors
+    tensors_to_load = torch.load(load_path)
+    
+    # Traverse through model parameters and load tensors from the list
+    for param_name, param_tensor in model.named_parameters():
+        if param_tensor.requires_grad:
+            param_tensor.data = tensors_to_load.pop(0).data
+
 newcolors = np.vstack(
     (
         np.flipud(mpl.colormaps['magma'](np.linspace(0, 1, 128))),
