b299f329c92dc18a0b5a1b98392c0d47f14434b0
Haowen Zhou <hzhou7@caltech.edu>
2023-07-05 21:59:54 -0700

    Light-field and normal variance AIF methods

--- FPM_AIF.py
+++ FPM_AIF.py
@@ -42,8 +42,69 @@ class LightField_AIF(nn.Module):
         im_depth = torch.sum(w_sharp * depth.unsqueeze(1).unsqueeze(1).unsqueeze(1), dim=0) / torch.sum(w_sharp, dim=0)
         return im_AIF, im_depth
 
+# Define balance map for normal variance method
+def CreateBalanceMap(m, patchsize, patchpace):
+    Iternum = int((m - patchsize) / patchpace + 1)
+    balancemap = np.zeros((m, m))
+    
+    for i in range(Iternum):
+        for j in range(Iternum):
+            start_ud = i * patchpace
+            end_ud = start_ud + patchsize
+            start_lr = j * patchpace
+            end_lr = start_lr + patchsize
+            
+            balancemap[start_ud:end_ud, start_lr:end_lr] += np.ones((patchsize, patchsize))
+    
+    balancemap = 1 / balancemap
+    
+    return Iternum, balancemap
+
+# AIF Normal Variance method
+def AIF_NV(imgs_gt):
+    m, n, framenum = imgs_gt.shape
+    
+    o_sum = np.mean(imgs_gt, axis=2)
+    
+    intensity, edges = np.histogram(np.abs(o_sum), bins='auto')
+    pos_i = np.argmax(intensity)
+    background = edges[pos_i]
+    
+    o_fusion = np.zeros((m, n))
+    
+    patchsize = 50
+    patchpace = 10
+    Iternum, balancemap = CreateBalanceMap(m, patchsize, patchpace)
+    
+    for i in range(Iternum):
+        for j in range(Iternum):
+            NV = []
+            for k in range(framenum):
+                start_ud = i * patchpace 
+                end_ud = start_ud + patchsize
+                start_lr = j * patchpace 
+                end_lr = start_lr + patchsize
+                
+                o_cropped = imgs_gt[start_ud:end_ud, start_lr:end_lr, k]
+                
+                # Rule out FPM artifacts (seems not necessary for fluorescence imaging)
+                if background > 0.1:
+                    o_cropped[o_cropped > background] = background
+                
+                o_set[start_ud:end_ud, start_lr:end_lr, k] = o_cropped
+                
+                mu = np.mean(o_cropped)
+                NV.append(np.sum((o_cropped - mu) ** 2) / (patchsize ** 2 * mu))
+            
+            loc_info = np.argmax(NV)
+            o_fusion[start_ud:end_ud, start_lr:end_lr] += imgs_gt[start_ud:end_ud, start_lr:end_lr, loc_info]
+    
+    o_fusion_balanced = o_fusion * balancemap
+    return o_fusion_balanced
 
 if __name__ == "__main__":
+
+    #  ~ 6 minutes to run the whole script
 
     # Parameters
     num_feats = 32
@@ -123,19 +184,20 @@ if __name__ == "__main__":
     # Save amplitude 
     imgs = (_imgs - _imgs.min()) / (_imgs.max() - _imgs.min())
 
+    ## Light field method
 
     # All-in-focus Section
-    sigma1 = 64                      ################### These two are important and need to addjust
+    sigma1 = 64                      ################### These two are important and need to adjust
     sigma2 = 10
     
     # load AIF model
-    AIF = LightField_AIF(sigma1=sigma1, sigma2=sigma2).to(device)
+    AIF_LF = LightField_AIF(sigma1=sigma1, sigma2=sigma2).to(device)
 
     # AIF FPM-INR
     start_time = time.time()
-    img_AIF,img_Depth = AIF(imgs.unsqueeze(1),dz)
+    img_AIF,img_Depth = AIF_LF(imgs.unsqueeze(1),dz)
     end_time = time.time()
-    print('FPM-INR AIF time elpased: ', np.round(end_time - start_time,2),'s')
+    print('FPM-INR AIF_LF time elpased: ', np.round(end_time - start_time,2),'s')
     
     # Load ground truth
     o_set  = np.abs(mat73.loadmat('data/20190113_Thyroid_NB11796113_pap_5_b_stack_0219.mat')['o_set']).astype('float32')
@@ -144,9 +206,9 @@ if __name__ == "__main__":
 
     # All-in-focus FPM
     start_time = time.time()
-    img_AIF_gt,img_Depth_gt = AIF(imgs_gt.unsqueeze(1),dz)
+    img_AIF_gt,img_Depth_gt = AIF_LF(imgs_gt.unsqueeze(1),dz)
     end_time = time.time()
-    print('FPM AIF Time elpased: ', np.round(end_time - start_time,2),'s')
+    print('FPM AIF_LF Time elpased: ', np.round(end_time - start_time,2),'s')
     
     # Calculate PSNR
     mse_loss = F.mse_loss(img_AIF_gt, img_AIF)
@@ -157,15 +219,15 @@ if __name__ == "__main__":
     print('AIF PSNR: ', np.round(AIF_psnr,2), 'dB')
     print('Depth Error: ', np.round(Depth_error,2), 'um with brightfield microscope DOF: ', np.round(DOF,2), 'um')
 
-    # In[] Plot
+    # Plot
     fig, ax = plt.subplots(1, 2)
     fig.set_dpi(600)
     cmap = 'gray'
     ax[0].imshow(img_AIF_gt[0].detach().cpu().numpy(), cmap=cmap)
-    ax[0].set_title('FPM AIF Image')
+    ax[0].set_title('FPM AIF_LF Image')
     ax[0].axis('off')
     ax[1].imshow(img_AIF[0].detach().cpu().numpy(), cmap=cmap)
-    ax[1].set_title('FPM-INR AIF Image')
+    ax[1].set_title('FPM-INR AIF_LF Image')
     ax[1].axis('off')
     
     
@@ -173,9 +235,40 @@ if __name__ == "__main__":
     fig, ax = plt.subplots(1, 2)
     fig.set_dpi(600)
     ax[0].imshow(img_Depth_gt[0].detach().cpu().numpy(), cmap=cmap)
-    ax[0].set_title('FPM Depth Map')
+    ax[0].set_title('FPM LF Depth Map')
     ax[0].axis('off')
     ax[1].imshow(img_Depth[0].detach().cpu().numpy(), cmap=cmap)
-    ax[1].set_title('FPM-INR Depth Map')
+    ax[1].set_title('FPM-INR LF Depth Map')
     ax[1].axis('off')
     plt.show()
+
+
+    ## Normal Variance AIF method
+    start_time = time.time()
+    imgs_gt = (o_set - np.min(o_set)) / (np.max(o_set) - np.min(o_set))
+    img_AIFNV_gt = AIF_NV(imgs_gt)
+    end_time = time.time()
+    print('FPM-INR AIF_NV time elpased: ', np.round(end_time - start_time,2),'s')
+    
+    start_time = time.time()
+    imgs_AIF_INR = np.moveaxis(imgs.detach().cpu().numpy(),0,-1)
+    img_AIFNV = AIF_NV(imgs_AIF_INR)
+    end_time = time.time()
+    print('FPM-INR AIF_NV time elpased: ', np.round(end_time - start_time,2),'s')
+
+    fig, ax = plt.subplots(1, 2)
+    fig.set_dpi(400)
+    cmap = 'gray'
+    ax[0].imshow(img_AIFNV_gt, cmap=cmap)
+    ax[0].set_title('FPM AIF_NV Image')
+    ax[0].axis('off')
+    ax[1].imshow(img_AIFNV, cmap=cmap)
+    ax[1].set_title('FPM-INR AIF_NV Image')
+    ax[1].axis('off')
+    
+
+    mse_loss = F.mse_loss(torch.from_numpy(img_AIFNV_gt).to(device), torch.from_numpy(img_AIFNV).to(device))
+    psnr = 10 * -torch.log10(mse_loss).item()
+    
+    print('MSE: ',np.round(mse_loss.item(),5))
+    print('PSNR: ', np.round(psnr,2),'dB')
