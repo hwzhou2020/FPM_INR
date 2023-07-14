8816b5459daa0fc4c7b3064f086319fab0e62e2e
brandonyfeng <fengyushan1002@gmail.com>
2023-07-13 22:14:24 -0700

    Update network.py

--- network.py
+++ network.py
@@ -164,3 +164,91 @@ class FullModel(nn.Module):
         img_imag = self.img_imag(dz)
 
         return img_real.view(b, w, h), img_imag.view(b, w, h)
+
+
+class G_Tensor3D_v2(nn.Module):
+    def __init__(self, x_dim, y_dim, z_dim, z_min, z_max, num_feats = 32, ds_factor = 1):
+        super().__init__()
+        self.renderer_1 = G_Renderer(in_dim=num_feats, out_dim=1)
+        self.renderer_2 = G_Renderer(in_dim=num_feats, out_dim=1)
+
+        self.x_dim, self.y_dim = x_dim, y_dim
+
+        x_mode, y_mode = 512, 512
+        self.num_feats = num_feats
+
+        self.data = nn.Parameter(
+            2e-4 * torch.randn((x_mode, y_mode, num_feats)), requires_grad=True)
+
+        half_dx, half_dy =  0.5 / x_dim, 0.5 / y_dim
+        xs = torch.linspace(half_dx, 1-half_dx, x_dim)
+        ys = torch.linspace(half_dx, 1-half_dy, y_dim)
+        xv, yv = torch.meshgrid([xs, ys], indexing="ij")
+        xy = torch.stack((yv.flatten(), xv.flatten())).t()
+
+        xs = xy * torch.tensor([x_mode, y_mode], device=xs.device).float()
+        indices = xs.long()
+        self.lerp_weights = nn.Parameter(xs - indices.float(), requires_grad=False)
+
+        self.x0 = nn.Parameter(indices[:, 0].clamp(min=0, max=x_mode-1), requires_grad=False)
+        self.y0 = nn.Parameter(indices[:, 1].clamp(min=0, max=y_mode-1), requires_grad=False)
+        self.x1 = nn.Parameter((self.x0 + 1).clamp(max=x_mode-1), requires_grad=False)
+        self.y1 = nn.Parameter((self.y0 + 1).clamp(max=y_mode-1), requires_grad=False)
+
+        self.z_dim = z_dim
+        z_mode = z_dim // ds_factor
+        self.z_data = nn.Parameter(
+            torch.randn((z_mode, num_feats)), requires_grad=True)
+        self.z_min = z_min
+        self.z_max = z_max
+
+    def sample(self, z):
+        z = self.normalize_z(z)
+        z0 = z.long().clamp(min=0, max=self.z_dim-1)
+        z1 = (z0 + 1).clamp(max=self.z_dim-1)
+        zlerp_weights = (z - z.long().float())[:, None]
+
+        xy_feat = (
+				self.data[self.y0, self.x0] * (1.0 - self.lerp_weights[:,0:1]) * (1.0 - self.lerp_weights[:,1:2]) +
+				self.data[self.y0, self.x1] * self.lerp_weights[:,0:1] * (1.0 - self.lerp_weights[:,1:2]) +
+				self.data[self.y1, self.x0] * (1.0 - self.lerp_weights[:,0:1]) * self.lerp_weights[:,1:2] +
+				self.data[self.y1, self.x1] * self.lerp_weights[:,0:1] * self.lerp_weights[:,1:2]
+		)
+
+        z_feat = self.z_data[z0] * (1.0 - zlerp_weights) + self.z_data[z1] * zlerp_weights
+        z_feat = z_feat[:, None].repeat(1, xy_feat.shape[0], 1)
+
+        feat = xy_feat[None].repeat(z.shape[0], 1, 1) * z_feat
+
+        return feat
+
+    def forward(self, z):
+        feat = self.sample(z)
+        b, n, c = feat.shape
+
+        out_1 = self.renderer_1(feat.view(-1, c))
+        out_2 = self.renderer_2(feat.view(-1, c))
+        out_1 = out_1.view(b, n, out_1.shape[-1])
+        out_2 = out_2.view(b, n, out_2.shape[-1])
+
+        return out_1, out_2
+
+
+
+class FullModel_v2(nn.Module):
+    def __init__(self, w, h, num_feats, Pupil0, n_views, z_min, z_max):
+        super().__init__()
+        self.net = G_Tensor3D_v2(w, h, z_dim=5, z_min=z_min, z_max=z_max, num_feats=num_feats)
+
+        self.w = w
+        self.h = h
+
+    def forward(self, dz):
+        w, h = self.w, self.h
+        b = dz.shape[0]
+        img_ampli, img_phase = self.net(dz)
+        img_ampli = img_ampli.view(b, w, h)
+        img_phase = img_phase.view(b, w, h)
+
+        return img_ampli, img_phase
+
