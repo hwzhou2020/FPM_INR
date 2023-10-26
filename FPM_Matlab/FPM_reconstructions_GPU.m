1ee466aed0aab0832a49f1507483e40d7145469c
Haowen Zhou <hzhou7@caltech.edu>
2023-10-26 11:23:28 -0700

    Matlab code up to date

--- FPM_Matlab/FPM_reconstructions_GPU.m
+++ FPM_Matlab/FPM_reconstructions_GPU.m
@@ -1,25 +1,63 @@
-clear all; clc;
-%  close all;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% FPM reconstruction with first-order optimization method and pupil 
+% correction functionality
+
+% The code comes with GPU processing
+% Matlab parallel computing toolbox is required
+
+% If you do not have a GPU and do not want to use GPU, please delete
+% gpuArray() and gather() in the code
+
+
+% This code is realted to the paper:
+% FPM-INR: Fourier ptychographic microscopy image stack reconstruction using implicit neural representations
+
+% GitHub:
+% https://github.com/hwzhou2020/FPM_INR
+
+% Written by Haowen Zhou and Mingshu Liang @ Caltech Biophotonics Lab 
+% Last modified on 10/26/2023 
+% Contact:
+% hzhou7@caltech.edu (Haowen Zhou) 
+% mlliang@caltech.edu (Mingshu Liang)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Code starts here
+clear all; clc;close all;
+
+
 %% Operator
 F  = @(x) fftshift(fft2(ifftshift(x)));
 Ft = @(x) ifftshift(ifft2(fftshift(x)));
 logamp = @(x) log10(abs(x)+1);
+
 %%
-sample_name = 'NSCLC_test_10X';
+
+if_3D = false;
+use_PupilCorrection = false;
 color = 'r';
-filename = [sample_name,'_',color,'_FPM.mat'];
+
+if if_3D:
+    sample_name = 'WU1005'; % 'Siemens', 'sheepblood'
+    dz_set = 0;
+else:
+    sample_name = 'BloodSmearTilt';
+    dz_set = -20:0.25:20;
+
+
+filename = [sample_name,'_',color,'.mat'];
 
 % load raw data
-fileName = ['FPM_Data/',sample_name,'/Preprocessed_', filename];
+fileName = ['../data/',sample_name,'/', filename];
 load(fileName);
 
-dz_set = 0%15:20 %-20:0.25:20;
-
 %% Set all necessary parameters (Unit: um)
-NA = na_cal; % objective NA
-mag = mag; % objective magnification
-pixel_size = 3.45; % camera pixel pitch
-D_led = 4*1000; % adjacent LED distance
+NA = na_cal;            % objective NA
+mag = mag;              % objective magnification
+pixel_size = 3.45;      % camera pixel pitch
+D_led = 4*1000;         % adjacent LED distance
 D_pixel = pixel_size/mag;
 
 NAshift_x = -na_calib(:,1)'; 
@@ -36,10 +74,11 @@ switch color
         lambda = 0.471;
 end
 
+% Compute k-vector
 k0 = 2*pi/lambda;
 kmax = NA*k0; % maximum spatial frequency 
 
-%% 
+%%
 I = double(I_low);
 
 clear('I_low');
@@ -48,7 +87,7 @@ clear('I_low');
 MAGimg = 2; % upsampling rate
 MM=M*MAGimg;NN=N*MAGimg;
 
-Niter1 = 100;
+Niter = 25;
 
 x=0;y=0; % field of view center shift
 
@@ -120,90 +159,91 @@ PupilSUM=O.*0;
 alpha=0.5;beta=0.1;
 
 % tic;
-for iter=1:Niter1
+for iter=1:Niter
     error_now=0;
     
     count = 0;
-    for led_num=1:ID_len
+        for led_num=1:ID_len
 
-        uo=ledpos_true(led_num,1);
-        vo=ledpos_true(led_num,2);
-        
-        OP_bef=O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))./(MAGimg^2).*Pupil.*dfmask;
-        o_bef=ifft2(fftshift(OP_bef));
-        oI_bef=abs(o_bef).^2;
-        
-        oI_cap=gpuArray(Isum(:,:,led_num));
-        if ((mean2(oI_cap)>0.1) && (mean2(oI_bef)>0.1)) || ((mean2(oI_cap)<0.1) && (mean2(oI_bef)<0.1))
-            o_aft=sqrt(oI_cap)./sqrt(oI_bef).*o_bef;
-        else
-            o_aft=o_bef;
-        end
+            uo=ledpos_true(led_num,1);
+            vo=ledpos_true(led_num,2);
+            
+            OP_bef=O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))./(MAGimg^2).*Pupil.*dfmask;
+            o_bef=ifft2(fftshift(OP_bef));
+            oI_bef=abs(o_bef).^2;
+            
+            oI_cap=gpuArray(Isum(:,:,led_num));
+            if ((mean2(oI_cap)>0.1) && (mean2(oI_bef)>0.1)) || ((mean2(oI_cap)<0.1) && (mean2(oI_bef)<0.1))
+                o_aft=sqrt(oI_cap)./sqrt(oI_bef).*o_bef;
+            else
+                o_aft=o_bef;
+            end
 
-        OP_aft=fftshift(fft2(o_aft));
-        
-        OP_diff=(OP_aft-OP_bef);
-        
-        temp=O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2));
-        O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))=temp+...
-            alpha*abs(Pupil).*conj(Pupil).*OP_diff./max(max(abs(Pupil)))./(abs(Pupil).^2+1).*conj(dfmask);
-        Pupil=Pupil+...
-            beta*(abs(OP_bef).*conj(OP_bef)).*OP_diff./max(max(abs(temp)))./(abs(OP_bef).^2+1000).*Pupil0;
-%         Pupil=Pupil0.*exp(1i.*angle(Pupil));
-        
-        
-        
-        
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        if (iter==1)
-            PupilSUM((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))=Pupil0+...
-                PupilSUM((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2)).*(ones(M,N)-Pupil0);
-         else
-            O=O.*PupilSUM;
+            OP_aft=fftshift(fft2(o_aft));
+            
+            OP_diff=(OP_aft-OP_bef);
+            
+            temp=O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2));
+            O((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))=temp+...
+                alpha*abs(Pupil).*conj(Pupil).*OP_diff./max(max(abs(Pupil)))./(abs(Pupil).^2+1).*conj(dfmask);
+
+            if use_PupilCorrection:
+                Pupil=Pupil+...
+                    beta*(abs(OP_bef).*conj(OP_bef)).*OP_diff./max(max(abs(temp)))./(abs(OP_bef).^2+1000).*Pupil0;
+            else:
+                Pupil=Pupil0.*exp(1i.*angle(Pupil));
+            end
+            
+            if (iter==1)
+                PupilSUM((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2))=Pupil0+...
+                    PupilSUM((vo-M/2):(vo-1+M/2),(uo-N/2):(uo-1+N/2)).*(ones(M,N)-Pupil0);
+            else
+                O=O.*PupilSUM;
+            end
+            error_now=error_now+sum(sum((abs(o_bef)-sqrt(oI_cap)).^2));
         end
-        error_now=error_now+sum(sum((abs(o_bef)-sqrt(oI_cap)).^2));
+    
+        if  iter>1 && (error_bef-error_now)/error_bef<0.01 
+            
+            % Reduce the stepsize when no sufficient progress is made
+            alpha = alpha/2;
+            beta=beta/2;
+            % Stop the iteration when Alpha is less than 0.001(convergenced)
+            if(alpha<0.00001)
+                alpha = 0;
+            end
+            
+        end
+        
+        error_bef = error_now;
+        
+
+        spectrum=log(abs(O)+1);
+        o=ifft2(fftshift(O));
+        oI=abs(o).^2;
+
+        % subplot(1,2,1)
+        % imshow(spectrum,[]);
+        % title('Fourier spectrum');
+        % subplot(1,2,2)
+        % imshow(oI,[]);
+        % title(['Iteration No. = ',int2str(iter), '  \alpha = ',num2str(alpha)]);
+        % pause(0.1)
+        
+        if(alpha == 0)
+            break; end
     end
-   
-    if  iter>1 && (error_bef-error_now)/error_bef<0.01 
-        
-        % Reduce the stepsize when no sufficient progress is made
-        alpha = alpha/2;
-        beta=beta/2;
-        % Stop the iteration when Alpha is less than 0.001(convergenced)
-        if(alpha<0.00001)
-            alpha = 0;
-        end
-        
-    end
-    
-    error_bef = error_now;
-    
 
-    spectrum=log(abs(O)+1);
-    o=ifft2(fftshift(O));
-    oI=abs(o).^2;
+    time = toc
+    o_set(:,:,dz) = o;
+    oP=angle(o);
 
-    subplot(1,2,1)
-    imshow(spectrum,[]);
-    title('Fourier spectrum');
-    subplot(1,2,2)
-    imshow(oI,[]);
-    title(['Iteration No. = ',int2str(iter), '  \alpha = ',num2str(alpha)]);
-    pause(0.1)
-    
-    if(alpha == 0)
-        break; end
-end
-time = toc
-o_set(:,:,dz) = o;
-oP=angle(o);
-
-%%
-figure(1000);
-subplot(1,2,1), imagesc(oI/max(oI(:))), axis image, colormap gray; axis off; title('Amplitude');colorbar;
-subplot(1,2,2), imagesc(oP), axis image, colormap gray; axis off; caxis([-pi,pi]);title('Phase');colorbar;
-% saveas(1000+2*dz,['img_',num2str(dz),'.tif']);
+    %%
+    figure(1000);
+    subplot(1,2,1), imagesc(oI/max(oI(:))), axis image, colormap gray; axis off; title('Amplitude');colorbar;
+    subplot(1,2,2), imagesc(oP), axis image, colormap gray; axis off; caxis([-pi,pi]);title('Phase');colorbar;
+    % saveas(1000+2*dz,['img_',num2str(dz),'.tif']);
 end
 times = toc;
 o_set = gather(o_set);
-% save([sample_name,'_',color,'_stack.mat'],'o_set','dz_set','-v7.3')
+save([sample_name,'_',color,'_FPM.mat'],'o_set','dz_set','-v7.3')
