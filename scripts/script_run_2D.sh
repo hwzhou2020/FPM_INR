e0e1d7051648d136e3c0a8b6cef5fcaa725ab1ce
Haowen Zhou <hzhou7@caltech.edu>
2023-10-26 12:01:50 -0700

    bash files for running python scripts.

--- scripts/script_run_2D.sh
+++ scripts/script_run_2D.sh
@@ -1,1 +1,1 @@
-python ../FPM_INR.py --num_epochs 15 --lr_decay_step 6 --num_feats 32 --amp --sample "Siemens" --color "r"
+python ../FPM_INR.py --is_system "Linux" --num_epochs 15 --lr_decay_step 6 --num_feats 32 --amp --sample "Siemens" --color "r"
--- scripts/script_run_3D.sh
+++ scripts/script_run_3D.sh
@@ -1,1 +1,1 @@
-python ../FPM_INR.py --fit_3D --num_epochs 15 --lr_decay_step 6 --num_feats 32 --amp --sample "BloodSmearTilt" --color "g"
+python ../FPM_INR.py --is_system "Linux" --fit_3D --num_epochs 15 --lr_decay_step 6 --num_feats 32 --amp --sample "BloodSmearTilt" --color "g"
