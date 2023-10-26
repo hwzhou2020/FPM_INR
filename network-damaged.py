de53a1a17c3dbca6dbb598860b6ac1ae4281dd9c
Haowen Zhou <hzhou7@caltech.edu>
2023-10-25 23:57:29 -0700

    damage network.py file for now. Will upload the full version upon the acceptance of the paper.

---
+++ network-damaged.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+
+class G_Renderer(nn.Module):
+    None
+
+class G_FeatureTensor(nn.Module):
+    None
+
+
+class G_Tensor(G_FeatureTensor):
+    None
+
+
+class FullModel(nn.Module):
+    None
